5ea848d6dcbf08fc842a9701ec3d4dd90e85ab63
Patrick Osborne <posborne@greenenergycorp.com>
2012-02-23 19:17:59 -0700

    default logger name or tag in Android logging is now: iJetty

--- i-jetty/i-jetty-server/src/main/java/org/mortbay/ijetty/log/AndroidLog.java
+++ i-jetty/i-jetty-server/src/main/java/org/mortbay/ijetty/log/AndroidLog.java
@@ -20,7 +20,7 @@ import org.slf4j.LoggerFactory;
 
 public class AndroidLog implements org.eclipse.jetty.util.log.Logger
 {
-    private static final Logger LOGGER = LoggerFactory.getLogger( "Jetty" );
+    private static final Logger LOGGER = LoggerFactory.getLogger( "iJetty" );
 
     private final Logger logger;
 
